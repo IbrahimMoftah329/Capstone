2d6816927af4e0c8011fbcc8ffc2708408ca5961
ninapham11 <nina.pham08@myhunter.cuny.edu>
2024-09-26 13:27:31 -0400

    restored the navbar

--- Frontend/src/components/Navbar.jsx
+++ Frontend/src/components/Navbar.jsx
@@ -1,38 +1,13 @@
-import React, { useState } from 'react';
-import logo from '../pics/logo.png'
-import { AiOutlineClose, AiOutlineMenu } from 'react-icons/ai';
+import React from "react"
 
-const Navbar = () => {
-  const [nav, setNav] = useState(false);
+const NavBar = () => {
 
-  const handleNav = () => {
-    setNav(!nav);
-  };
 
-  return (
-    <div className='flex justify-between items-center h-24 max-w-[1240px] mx-auto px-4 text-white'>
-      <img className='w-[50px] mx-auto my-4' src={logo} alt='/' />
-      <h1 className='w-full font-serif text-3xl font-bold text-[#124883]'>Cardmates</h1>
-      <ul className='hidden md:flex'>
-        <li className='p-4 text-black'>Home</li>
-        <li className='p-4 text-black'>Company</li>
-        <li className='p-4 text-black'>Resources</li>
-        <li className='p-4 text-black'>About</li>
-        <li className='p-4 text-black'>Contact</li>
-      </ul>
-      <div onClick={handleNav} className='block md:hidden'>
-          {nav ? <AiOutlineClose size={20}/> : <AiOutlineMenu size={20} />}
-      </div>
-      <ul className={nav ? 'fixed left-0 top-0 w-[60%] h-full border-r border-r-gray-900 bg-[#000300] ease-in-out duration-500' : 'ease-in-out duration-500 fixed left-[-100%]'}>
-        <h1 className='w-full text-3xl font-bold text-[#00df9a] m-4'>REACT.</h1>
-          <li className='p-4 border-b border-gray-600'>Home</li>
-          <li className='p-4 border-b border-gray-600'>Company</li>
-          <li className='p-4 border-b border-gray-600'>Resources</li>
-          <li className='p-4 border-b border-gray-600'>About</li>
-          <li className='p-4'>Contact</li>
-      </ul>
-    </div>
-  );
-};
 
-export default Navbar;
+
+
+
+
+
+
+}
